254e8718113107268c492da4e5a95ad769d23ce2
Artem <artden1mc@gmail.com>
2020-07-08 19:36:41 +0300

    creater form menu

---
+++ GAMETARAKAN/Menu.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace GAMETARAKAN
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.level = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // level
+            // 
+            this.level.Location = new System.Drawing.Point(348, 87);
+            this.level.Name = "level";
+            this.level.Size = new System.Drawing.Size(75, 23);
+            this.level.TabIndex = 0;
+            this.level.Text = "button1";
+            this.level.UseVisualStyleBackColor = true;
+            this.level.Click += new System.EventHandler(this.level_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.level);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button level;
+    }
+}
--- GAMETARAKAN/Program.cs
+++ GAMETARAKAN/Program.cs
@@ -16,7 +16,7 @@ namespace GAMETARAKAN
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Menu());
         }
     }
 }
